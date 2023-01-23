e368208229bef5165626a30ce872b5a0d74bc0fe
Colby Neves <colby.neves@smail.astate.edu>
2023-01-23 09:43:11 -0600

    1/23/2023 Notes

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,14 @@ function getRoot(req: Request, res: Response): void{
     res.send('Hello, from Colby!');
 }
 
+app.get('/', getRoot);
+
 function handleListenEvent (): void{
+    //I can write any code that requires a bound port
     console.log(`Listening on port http://${ip.address()}:${PORT}`);
 }
 
-app.get('/', getRoot); 
-app.listen(PORT, handleListenEvent);
+
+app.listen(PORT, handleListenEvent); //turns on server at port specified; keeps it on for requests;
+//I have no idea if the port has been bound yet
+console.log('The listen function just executed.');
