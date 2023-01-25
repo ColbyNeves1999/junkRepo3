abdea9f1a9f5dc88d289e6494d4dfbd149ad2050
Colby Neves <colby.neves@smail.astate.edu>
2023-01-25 09:08:04 -0600

    1/23/23 Class Code

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,13 @@ import ip from 'ip';
 const app: Express = express();
 const PORT = 8191;
 
+
+const data = {
+    'Arkansas': 'Little Rock',
+    'Texa': 'Austin',
+    'Idaho': 'Salem',
+};
+
 function getRoot(req: Request, res: Response): void{
     res.send('Hello, from Colby!');
 }
@@ -15,6 +22,16 @@ function handleListenEvent (): void{
     console.log(`Listening on port http://${ip.address()}:${PORT}`);
 }
 
+function getCapital(req: Request, res: Response): void {
+    if(req.query.sate) {
+        res.json({capital: data[req.query.state]});
+    }else{
+    res.json(data);
+    }
+}
+
+app.get('/', getRoot);
+app.get('/capital', getCapital);
 
 app.listen(PORT, handleListenEvent); //turns on server at port specified; keeps it on for requests;
 //I have no idea if the port has been bound yet
